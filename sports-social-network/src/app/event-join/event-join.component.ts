ff5f99b4aed182c6c30f2f6929b302dba334099e
DELL-XPS\Ierem <ieremiasviorel@yahoo.com>
2019-01-04 18:44:18 +0200

    Fix filter

--- sports-social-network/src/app/event-join/event-join.component.ts
+++ sports-social-network/src/app/event-join/event-join.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 import { EventsService } from '../services/events.service';
 import { Event } from '../models/event';
 
 const SPORTS = [
-  '', 'Fotbal', 'Baschet'
+  '', 'FOTBAL', 'BASKET', 'BOWLING'
 ];
 
 const SKILL = [
@@ -23,8 +23,12 @@ export class EventJoinComponent implements OnInit {
   sports = SPORTS;
   skill = SKILL;
   selectedEvent: null;
-  prefSport = '';
-  prefSkill = '';
+
+  prefSport: string;
+  prefSkill: string;
+  prefPrice: string;
+  prefParticipantsNr: string;
+
   searched: string[] = [];
 
   constructor(
@@ -49,7 +53,6 @@ export class EventJoinComponent implements OnInit {
 
   onJoinEvent() {
     if (this.selectedEvent != null) {
-      console.log(this.selectedEvent.name);
       for (let i = 0; i < this.events.length; i++) {
         if (this.events[i] === this.selectedEvent) {
           this.events.splice(i, 1);
@@ -77,31 +80,37 @@ export class EventJoinComponent implements OnInit {
     }
   }
 
-  onApplyFilters(prefPrice, prefPart) {
-    if ( !(this.prefSport === '')) {
-      this.events.filter(event => event.category.toLowerCase() === this.prefSport.toLowerCase());
+  onApplyFilters() {
+    console.log(this.events);
+    console.log(this.prefSport);
+    if (this.prefSport) {
+      this.events = this.events.filter(ev => ev.category === this.prefSport);
     }
-    if ( !(this.prefSkill === '')) {
-      this.events.filter(event => event.skill.toLowerCase() === this.prefSkill.toLowerCase());
-    }
-    if ( !(prefPrice === 0)) {
-      console.log('3lung'+ this.events.length);
-      for (let i = 0; i < this.events.length; i++) {
-        console.log(this.events[i].name + 'muee'+i);
-        if (!(this.events[i].price < prefPrice)) {
-          this.events.splice(i, 1);
-        }
-      }
-    }
-    if ( !(prefPart === 0)) {
-      console.log('4lung'+ this.events.length);
-      for (let i = 0; i < this.events.length; i++) {
-        console.log(this.events[i].name + 'muee'+i);
-        if (!(this.events[i].participants < prefPart)) {
-          this.events.splice(i, 1);
-        }
-      }
-    }
+    console.log(this.events);
+    /*  if ( !(this.prefSport === '')) {
+       this.events.filter(event => event.category.toLowerCase() === this.prefSport.toLowerCase());
+     }
+     if ( !(this.prefSkill === '')) {
+       this.events.filter(event => event.skill.toLowerCase() === this.prefSkill.toLowerCase());
+     }
+     if ( !(prefPrice === 0)) {
+       console.log('3lung' + this.events.length);
+       for (let i = 0; i < this.events.length; i++) {
+         console.log(this.events[i].name + 'muee' + i);
+         if (!(this.events[i].price < prefPrice)) {
+           this.events.splice(i, 1);
+         }
+       }
+     }
+     if ( !(prefPart === 0)) {
+       console.log('4lung' + this.events.length);
+       for (let i = 0; i < this.events.length; i++) {
+         console.log(this.events[i].name + 'muee' + i);
+         if (!(this.events[i].participants < prefPart)) {
+           this.events.splice(i, 1);
+         }
+       }
+     } */
   }
 
   selectedSkill(value: any) {
