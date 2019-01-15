7d0dc59d22c744b63fbffb120be9d7b2004fa2d3
DELL-XPS\Ierem <ieremiasviorel@yahoo.com>
2019-01-15 19:09:00 +0200

    - complete ProfilePremiumPoints page - add Achievement class and Achievements service

--- sports-social-network/src/app/profile-premium-points/profile-premium-points.component.ts
+++ sports-social-network/src/app/profile-premium-points/profile-premium-points.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
 import { ScoredActiviesService } from '../services/scored-activies.service';
 import { ScoredActivity } from '../models/scored-activity';
+import { Achievement } from '../models/achievement';
+import { AchievementsService } from '../services/achievements.service';
 
 @Component({
   selector: 'app-profile-premium-points',
@@ -12,8 +15,11 @@ export class ProfilePremiumPointsComponent implements OnInit {
   scoredActivities: ScoredActivity[];
   selectedScoredActivity: ScoredActivity;
 
+  achievements: Achievement[];
+
   constructor(
-    private scoredActivitiesService: ScoredActiviesService
+    private scoredActivitiesService: ScoredActiviesService,
+    private achievementsSservice: AchievementsService
   ) { }
 
   ngOnInit() {
@@ -21,10 +27,14 @@ export class ProfilePremiumPointsComponent implements OnInit {
       .subscribe((scoredActivities: ScoredActivity[]) => {
         this.scoredActivities = scoredActivities;
       });
+
+    this.achievementsSservice.getUserAchievements()
+      .subscribe((achievements: Achievement[]) => {
+        this.achievements = achievements;
+      });
   }
 
   selectScoredActivity(scoredActivity: ScoredActivity): void {
     this.selectedScoredActivity = scoredActivity;
   }
-
 }
